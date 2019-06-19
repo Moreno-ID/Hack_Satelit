68fdab9aea73e4ffc82670b81236f27776ec96af
Be4Self <50474363+Moreno-ID@users.noreply.github.com>
2019-06-19 10:28:06 +0000

    Add files via upload

---
+++ halit.sh
@@ -0,0 +1,252 @@
+#!/bin/bash
+#RecodeGakBuatLuPro
+#CodeByBe4Self
+clear
+bi='\033[34;1m' #biru
+ij='\033[32;1m' #ijo
+pr='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+or='\033[1;38;5;208m' #Orange
+echo 'Ingat Ini Tools Buat Prank Temen Lu'
+echo 'Bahwa Lu Bisa Mindahin Satelit Awokwokwok'
+sleep 1
+
+echo "---------------------------------------------------------" |lolcat
+toilet -f pagga "        Satelit      " -F gay |lolcat
+echo "---------------------------------------------------------" |lolcat
+echo $me'[+]─────────────────────────────────────────────────────[+]'
+echo $ij' |  •••••••••••  |Termux[Terminal Linux]|  ••••••••••••  |'
+echo $pu' | ─────────────────-─────────────────────────────────── |'
+echo $me' | VERSION Termux: 5.0.7                                 |'
+echo $me' | Name          : Be4Self                               |'
+echo $me' | CodeName      : Be4xSelf                              |'
+echo $me' | WhatsApp      : +6285332814651                        |'
+echo $pu' | Instagram     : @moreno_id                            |'
+echo $pu' | Github        : https://github.com/Moreno-ID          |'
+echo $pu' | Thanks To     : Termux[Terminal Linux]                |'
+echo $pu' | COPYRIGHT     : 2K19 Be4Self                          |'
+echo $me' | ─────────────────-─────────────────────────────────── |'
+echo $ij' |  •••••••••••  |Termux[Terminal Linux]|  ••••••••••••  |'
+echo $pu'[+]─────────────────────────────────────────────────────[+]'
+
+###################################################
+# CTRL + C
+###################################################
+trap ctrl_c INT
+ctrl_c() {
+clear
+echo $me"[+]> (Ctrl + C ) Detected, Trying To Exit ... "
+echo $cy"[+]> Thanks"
+sleep 1
+echo ""
+echo $pu"[+]> see you gaes :)..."
+sleep 1
+exit
+}
+
+echo ""
+echo $me'-───────────────────────────────────────────'
+echo $ij'             Menu Hack Satelit'
+echo $pu'-───────────────────────────────────────────'
+echo $pr'«1»' $bi'Satelit Palapa                          |'
+echo $pr'«2»' $ij'Satelit Nasa                            |'
+echo $pr'«3»' $cy'Satelit Telkom                          |'
+echo $pr'«4»' $me'Satelit Garuda                          |'
+echo $pr'«5»' $pu'Satelit Indostar                        |'
+echo $pr'«0»' $ku'Exit My Program                         |'
+echo $me'-───────────────────────────────────────────'
+echo $ij'             Menu Hack Satelit'
+echo $pu'-───────────────────────────────────────────'
+echo $bi'╭──'$ij'{'$pr'Pilih'$ku'⚔'$me'~'$ij'}'
+read -p '╰───────‹Be4Self›=»' peak;
+
+if [ $peak = 1 ]
+then
+echo $bi"╭──"$ij"{"$pr"Mau KeArah Mana?"$ij"}"
+read -p"╰───────‹Be4Self›=»" ara;
+sleep 1
+echo $pr"╭──"$me"{"$cy"Mau DiGeser Berapa Derajat?"$me"}"
+read -p"╰───────‹Be4Self›=»" raja;
+sleep 1
+echo $cy'Proses Ea XD'
+sleep 1
+echo $me'10%'
+sleep 1
+echo $pu'20%'
+sleep 1
+echo $ku'30%'
+sleep 1
+echo $or'40%'
+sleep 1
+echo $bi'50%'
+sleep 1
+echo $ij'60%'
+sleep 1
+echo $cy'70%'
+sleep 1
+echo $me'80%'
+sleep 1
+echo $pu'90%'
+sleep 1
+echo $ku'100%'
+sleep 1
+echo $or'Proses Done XD'
+sleep 1
+echo $bi'Satelit Palapa Berhasil DiGeser Ke Arah' $ara 'Sejauh' $raja 'Derajat'
+exit
+fi
+
+if [ $peak = 2 ]
+then
+echo $bi"╭──"$ij"{"$pr"Mau KeArah Mana?"$ij"}"
+read -p"╰───────‹Be4Self›=»" ara;
+sleep 1
+echo $pr"╭──"$me"{"$cy"Mau DiGeser Berapa Derajat?"$me"}"
+read -p"╰───────‹Be4Self›=»" raja;
+sleep 1
+echo $cy'Proses Ea XD'
+sleep 1
+echo $me'10%'
+sleep 1
+echo $pu'20%'
+sleep 1
+echo $ku'30%'
+sleep 1
+echo $or'40%'
+sleep 1
+echo $bi'50%'
+sleep 1
+echo $ij'60%'
+sleep 1
+echo $cy'70%'
+sleep 1
+echo $me'80%'
+sleep 1
+echo $pu'90%'
+sleep 1
+echo $ku'100%'
+sleep 1
+echo $or'Proses Done XD'
+sleep 1
+echo $bi'Satelit Nasa Berhasil DiGeser Ke Arah' $ara 'Sejauh' $raja 'Derajat'
+exit
+fi
+
+if [ $peak = 3 ]
+then
+echo $bi"╭──"$ij"{"$pr"Mau KeArah Mana?"$ij"}"
+read -p"╰───────‹Be4Self›=»" ara;
+sleep 1
+echo $pr"╭──"$me"{"$cy"Mau DiGeser Berapa Derajat?"$me"}"
+read -p"╰───────‹Be4Self›=»" raja;
+sleep 1
+echo $cy'Proses Ea XD'
+sleep 1
+echo $me'10%'
+sleep 1
+echo $pu'20%'
+sleep 1
+echo $ku'30%'
+sleep 1
+echo $or'40%'
+sleep 1
+echo $bi'50%'
+sleep 1
+echo $ij'60%'
+sleep 1
+echo $cy'70%'
+sleep 1
+echo $me'80%'
+sleep 1
+echo $pu'90%'
+sleep 1
+echo $ku'100%'
+sleep 1
+echo $or'Proses Done XD'
+sleep 1
+echo $bi'Satelit Telkom Berhasil DiGeser Ke Arah' $ara 'Sejauh' $raja 'Derajat'
+exit
+fi
+
+if [ $peak = 4 ]
+then
+echo $bi"╭──"$ij"{"$pr"Mau KeArah Mana?"$ij"}"
+read -p"╰───────‹Be4Self›=»" ara;
+sleep 1
+echo $pr"╭──"$me"{"$cy"Mau DiGeser Berapa Derajat?"$me"}"
+read -p"╰───────‹Be4Self›=»" raja;
+sleep 1
+echo $cy'Proses Ea XD'
+sleep 1
+echo $me'10%'
+sleep 1
+echo $pu'20%'
+sleep 1
+echo $ku'30%'
+sleep 1
+echo $or'40%'
+sleep 1
+echo $bi'50%'
+sleep 1
+echo $ij'60%'
+sleep 1
+echo $cy'70%'
+sleep 1
+echo $me'80%'
+sleep 1
+echo $pu'90%'
+sleep 1
+echo $ku'100%'
+sleep 1
+echo $or'Proses Done XD'
+sleep 1
+echo $bi'Satelit Garuda Berhasil DiGeser Ke Arah' $ara 'Sejauh' $raja 'Derajat'
+exit
+fi
+
+if [ $peak = 5 ]
+then
+echo $bi"╭──"$ij"{"$pr"Mau KeArah Mana?"$ij"}"
+read -p"╰───────‹Be4Self›=»" ara;
+sleep 1
+echo $pr"╭──"$me"{"$cy"Mau DiGeser Berapa Derajat?"$me"}"
+read -p"╰───────‹Be4Self›=»" raja;
+sleep 1
+echo $cy'Proses Ea XD'
+sleep 1
+echo $me'10%'
+sleep 1
+echo $pu'20%'
+sleep 1
+echo $ku'30%'
+sleep 1
+echo $or'40%'
+sleep 1
+echo $bi'50%'
+sleep 1
+echo $ij'60%'
+sleep 1
+echo $cy'70%'
+sleep 1
+echo $me'80%'
+sleep 1
+echo $pu'90%'
+sleep 1
+echo $ku'100%'
+sleep 1
+echo $or'Proses Done XD'
+sleep 1
+echo $bi'Satelit Indostar Berhasil DiGeser Ke Arah' $ara 'Sejauh' $raja 'Derajat'
+exit
+fi
+
+if [ $peak = 0 ]
+then
+echo 'Thanks Udah Make Tools Gua' |lolcat
+echo 'Jgn Lupa Share Tools Ini Ke Temen Lu' |lolcat
+echo 'FB : Moreno ID' |lolcat
+exit
+fi
